eee0d50f3c528fe09e257f26a958280f2b4a844c
ahhhh450 <srywa5018@gmail.com>
2026-01-07 21:03:53 +0700

    Add files via upload

---
+++ expense-tracker/js/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx3hx9kuenBtdN1KBJJnPwc2H9BjH2SZI",
+  authDomain: "my-account-book-b4635.firebaseapp.com",
+  projectId: "my-account-book-b4635",
+  storageBucket: "my-account-book-b4635.firebasestorage.app",
+  messagingSenderId: "1044755389880",
+  appId: "1:1044755389880:web:50dfe83f8ad363988b834d",
+  measurementId: "G-6H58C7HDQX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
